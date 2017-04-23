016fad1b4c3357d5fa6e8d3c3ae131667c9f9a99
Trevor <trevorhoward2@hotmail.com>
2017-04-22 18:36:41 -0600

    Generated menu controller and customized routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :sandwich
+  resources :menu, except: [:new, :create, :edit, :update, :destroy]
 
   root to: 'sandwich#show'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419012523) do
+ActiveRecord::Schema.define(version: 20170423001544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "menus", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "sandwiches", force: :cascade do |t|
     t.datetime "created_at", null: false
